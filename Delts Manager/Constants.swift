11d1c84426ae9d03af480a2c02f0749194c592f3
Juan Diego Fajardo <jfajardo@mit.edu>
2017-06-11 23:02:59 -0400

    Add User and Constants files.

---
+++ Delts Manager/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 6/11/17.
+//  Copyright © 2017 Delta Tau Delta. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    // Place constants here!
+}
---
+++ Delts Manager/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 6/11/17.
+//  Copyright © 2017 Delta Tau Delta. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    // Instance variables
+    let firstName : String
+    let lastName : String
+    var year : Int
+    let email : String
+    var duties : [String] // TODO: replace with [Duty]
+    var punts : [String] // TODO: replace with [Punt]
+    var partyDuties: [String] // TODO: replace with [PartyDuty]
+    
+    // Initialization
+    init(firstName: String, lastName: String, year: Int, email: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.year = year
+        self.email = email
+        
+        self.duties = []
+        self.punts = []
+        self.partyDuties = []
+    }
+}
