a3d544f2371137aa32b0c57e2aee99353bfdb61b
Tim Henry <timhenry@dhcp-18-189-60-84.dyn.mit.edu>
2017-06-11 23:22:05 -0400

    Added initial Punt class

---
+++ Delts Manager/Punt.swift
@@ -0,0 +1,59 @@
+//
+//  Punt.swift
+//  Delts Manager
+//
+//  Created by Tim Henry on 6/11/17.
+//  Copyright © 2017 Delta Tau Delta. All rights reserved.
+//
+
+import Foundation
+
+class Punt {
+    // Instance variables
+    var date : NSDate
+    var duty : Duty
+    let comment : String
+    var user : User
+    var giver : User
+    var madeUp : Bool
+    
+    // Initialization
+    init(giver : User, duty : Duty, comment : String) {
+        
+        self.date = Date()
+            
+        self.giver = giver
+        self.duty = duty
+        self.comment = comment
+        
+        self.madeUp = false
+    }
+    
+    func makeUp() -> Void {
+        self.madeUp = true
+    }
+    
+    func getDate() -> NSDate {
+        return self.date
+    }
+    
+    func getDuty() -> Duty {
+        return self.duty
+    }
+    
+    func getComment() -> String {
+        return self.comment
+    }
+    
+    func getUser() -> User {
+        return self.user
+    }
+    
+    func getGiver() -> User {
+        return self.giver
+    }
+    
+    func isMadeUp() -> Bool {
+        return self.madeUp
+    }
+}
