1a37aa7a9e338972521018048165e5242d4ea5e3
Tim Henry <timhenry@dhcp-18-189-60-84.dyn.MIT.EDU>
2017-06-13 00:39:13 -0400

    Changed to collection view controller for duties

---
+++ Delts Manager/DutyCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DutyCellTableViewCell.swift
+//  Delts Manager
+//
+//  Created by Tim Henry on 6/13/17.
+//  Copyright © 2017 Delta Tau Delta. All rights reserved.
+//
+
+import UIKit
+
+class DutyCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Delts Manager/DutyViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DutyViewController.swift
+//  Delts Manager
+//
+//  Created by Tim Henry on 6/13/17.
+//  Copyright © 2017 Delta Tau Delta. All rights reserved.
+//
+
+import Foundation
